6c578a62c6eb088e180a1efdab28801614b5ce92
amir-kamel <amir.kghabrial@gmail.com>
2025-04-28 01:01:42 +0300

    test connection between front and api back

--- Hotel_Reservation_API/Hotel_Reservation_API/settings.py
+++ Hotel_Reservation_API/Hotel_Reservation_API/settings.py
@@ -69,11 +69,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "https://hotelsreservationsystemapi-production.up.railway.app/",
-    "https://hotels-reservation-system-ui.vercel.app",
-]
+CORS_ALLOWED_ORIGINS = ['*']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
@@ -208,10 +204,6 @@ SIMPLE_JWT = {
     "TOKEN_BLACKLIST_ENABLED": True,
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "https://hotelsreservationsystemapi-production.up.railway.app",
-    "https://hotels-reservation-system-ui.vercel.app",
-]
+CORS_ALLOWED_ORIGINS = ['*']
 CORS_ALLOW_CREDENTIALS = True
 
